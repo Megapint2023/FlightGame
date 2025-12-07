7b55ab63a3bb838c803a0ad3b983b296d46ac116
Your Name <aleksi.ruotsalo@hotmail.fi>
2025-12-07 17:55:42 +0200

    LENON HINTA VÄLÄHDYS - LISÄTTY

--- JS/travel_to.js
+++ JS/travel_to.js
@@ -10,25 +10,32 @@ async function travel_to(airport) {
     })
     .then(response => response.json())
     .then(async data => {
-
+        // SÄÄN PÄIVITTÄMINEN
         let weatherText = "- sää ei saatavilla...";
-
         if (data.weather && data.weather.temp !== undefined) {
             const w = data.weather;
             const windText = (w.wind !== undefined) ? `, wind ${w.wind} m/s` : "";
             weatherText = `- ${w.temp.toFixed(1)}°C, ${w.description}${windText}`;
         }
-
         document.getElementById("weather").innerText = weatherText;
-        update_stats(data);
+        update_stats(data); // PÄIVITTYY STATUS PANEELIN JA SIJINTI ELEMNTIN
+
+        // VÄLÄHDYS KYSEISEN SIIRRON ELI LENNON HINTA
+        const lennonhinta = document.getElementById("lennonhinta");
+        lennonhinta.textContent = `-${data.ticket_cost} €`;
+        lennonhinta.classList.add("show");
+        setTimeout(() => lennonhinta.classList.remove("show"), 900);
+
+        // VÄLÄHDYS AINA KUN SAA PISTEEN
         if (data.found === true) {
-          const gain = document.getElementById("point_gain");
-          gain.classList.add("show");
-          setTimeout(() => gain.classList.remove("show"), 900);
-}
-        await updateHiddenLocation();
-        updateNavigator(airport.lat, airport.lon);
-    })
+        const gain = document.getElementById("point_gain");
+        gain.classList.add("show");
+        setTimeout(() => gain.classList.remove("show"), 900);
+    }
+
+    await updateHiddenLocation();
+    updateNavigator(airport.lat, airport.lon);
+})
     .catch(error => {
         console.error("Move failed:", error);
         document.getElementById("weather").innerText = "- sää ei saatavilla...";
